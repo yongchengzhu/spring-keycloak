d943dfeb6ed3560a50c7b2df891357979c73f114
yongchengzhu <yongchengzhu002@gmail.com>
2020-07-06 14:02:03 -0400

    create class for keycloak server config

---
+++ src/main/java/com/yongcheng/keycloakserver/config/EmbeddedKeycloakApplication.java
@@ -0,0 +1,10 @@
+package com.yongcheng.keycloakserver.config;
+
+import org.keycloak.services.resources.KeycloakApplication;
+
+public class EmbeddedKeycloakApplication extends KeycloakApplication {
+  
+  public EmbeddedKeycloakApplication() {
+    super();
+  }
+}
