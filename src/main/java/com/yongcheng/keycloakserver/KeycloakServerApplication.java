6c16bdf6b8e5d9683e9759f5f153e9fec3187f6c
yongchengzhu <yongchengzhu002@gmail.com>
2020-07-08 15:14:24 -0400

    implement keycloak's resteasy and simpleplatform providers

--- src/main/java/com/yongcheng/keycloakserver/KeycloakServerApplication.java
+++ src/main/java/com/yongcheng/keycloakserver/KeycloakServerApplication.java
@@ -2,12 +2,36 @@ package com.yongcheng.keycloakserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.liquibase.LiquibaseAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication
+import com.yongcheng.keycloakserver.config.KeycloakServerProperties;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.boot.autoconfigure.web.ServerProperties;
+
+@SpringBootApplication(exclude = LiquibaseAutoConfiguration.class)
 public class KeycloakServerApplication {
-
+  private static final Logger LOG = LoggerFactory.getLogger(KeycloakServerApplication.class);
 	public static void main(String[] args) {
 		SpringApplication.run(KeycloakServerApplication.class, args);
+  }
+  
+  // *** Requires spring-boot-starter-web dependency for ServerProperties.
+  @Bean
+	ApplicationListener<ApplicationReadyEvent> onApplicationReadyEventListener(ServerProperties serverProperties,
+			KeycloakServerProperties keycloakServerProperties) {
+
+		return (evt) -> {
+
+			Integer port = serverProperties.getPort();
+			String keycloakContextPath = keycloakServerProperties.getContextPath();
+
+			LOG.info("Embedded Keycloak started: http://localhost:{}{} to use keycloak", port, keycloakContextPath);
+		};
 	}
 
 }
