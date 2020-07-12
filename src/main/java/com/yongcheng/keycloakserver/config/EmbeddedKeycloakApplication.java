2e2a38d659b9b1f1c809fa8d579c2825e88e2ca7
yongchengzhu <yongchengzhu002@gmail.com>
2020-07-12 13:42:11 -0400

    create auth realm

--- src/main/java/com/yongcheng/keycloakserver/config/EmbeddedKeycloakApplication.java
+++ src/main/java/com/yongcheng/keycloakserver/config/EmbeddedKeycloakApplication.java
@@ -1,16 +1,22 @@
 package com.yongcheng.keycloakserver.config;
 
+import java.io.InputStream;
 import java.util.NoSuchElementException;
 
 import com.yongcheng.keycloakserver.config.KeycloakServerProperties.AdminUser;
 
 import org.keycloak.Config;
 import org.keycloak.models.KeycloakSession;
+import org.keycloak.representations.idm.RealmRepresentation;
 import org.keycloak.services.managers.ApplianceBootstrap;
+import org.keycloak.services.managers.RealmManager;
 import org.keycloak.services.resources.KeycloakApplication;
 import org.keycloak.services.util.JsonConfigProviderFactory;
+import org.keycloak.util.JsonSerialization;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 
 public class EmbeddedKeycloakApplication extends KeycloakApplication {
 
@@ -26,6 +32,7 @@ public class EmbeddedKeycloakApplication extends KeycloakApplication {
   public EmbeddedKeycloakApplication() {
     super();
     createMasterRealmAdminUser();
+    createAuthRealm();
   }
 
   private void createMasterRealmAdminUser() {
@@ -45,4 +52,31 @@ public class EmbeddedKeycloakApplication extends KeycloakApplication {
 
     session.close();
   }
+
+  private void createAuthRealm() {
+    KeycloakSession session = getSessionFactory().create();
+
+    try {
+      session.getTransactionManager().begin();
+
+      RealmManager manager = new RealmManager(session);
+      
+      String path = keycloakServerProperties.getRealmImportFile();
+      Resource realmImportFile = new ClassPathResource(path);
+      
+      InputStream bytes = realmImportFile.getInputStream();
+      Class<RealmRepresentation> type = RealmRepresentation.class;
+      RealmRepresentation rep = JsonSerialization.readValue(bytes, type);
+      
+      manager.importRealm(rep);
+      
+      session.getTransactionManager().commit();
+    } catch (Exception ex) {
+      LOG.warn("Failed to import Realm json file: {}", ex.getMessage());
+      session.getTransactionManager().rollback();
+    }
+
+    session.close();
+  }
+
 }
